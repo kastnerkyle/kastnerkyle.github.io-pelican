ae0d40814b199c0bf890ba1d68c428db1f82a049
Kyle Kastner <kastnerkyle@gmail.com>
2014-04-17 14:17:40 -0500

    Updated wavelet for image compression

--- content/2014-04-17-simple-page-rank.md
+++
@@ -1,6 +0,0 @@
-Title: Simple Page Rank 
-date: 2014-04-17 9:00
-comments: true
-slug: simple-page-rank 
-
-{% notebook ml_simple_pagerank.ipynb %}
--- content/downloads/notebooks/cleanup.sh
+++ content/downloads/notebooks/cleanup.sh
@@ -3,4 +3,9 @@ rm *.zip
 rm *.mat
 rm *.webm
 rm *.mp4
+rm *.csv
+rm *.gif
+rm *.gz
+rm *.tar
+rm *.pyc
 rm -rf Campus Curtain Bootstrap hall ShoppingMall WaterSurface Fountain Escalator
